d20a78cd86aa58dfe3dc680f03c9de208214f758
Tom Horan <t.v.horan@gmail.com>
2022-06-02 10:25:45 +0100

    Moving due date logic to server

---
+++ Todoish.ServiceInterface/DueDateService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using ServiceStack;
+using Todoish.ServiceModel;
+
+namespace Todoish.ServiceInterface;
+
+public class DueDateService : Service
+{
+    public object Any(DueDate request)
+    {
+        if (request.Title.Contains(" tom", StringComparison.OrdinalIgnoreCase))
+        {
+            return new DueDateResponse
+            {
+                Result = DateTime.Today + TimeSpan.FromDays(1)
+            };
+        }
+
+        return new DueDateResponse();
+    }
+}
---
+++ Todoish.ServiceModel/DueDate.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using ServiceStack;
+
+namespace Todoish.ServiceModel;
+
+[Route("/duedate")]
+public class DueDate : IReturn<DueDateResponse>
+{
+    public string Title { get; set; }   
+}
+
+public class DueDateResponse
+{
+    public DateTime? Result { get; set; }
+    public ResponseStatus ResponseStatus { get; set; }
+}
